85a43cd81ddcfe8026e59a47a2b79cd02e4c8ab3
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-04-04 23:43:20 +0530

    Created the api endpoint to update the todo

--- src/main/java/com/project/crudapplication/controller/TodoController.java
+++ src/main/java/com/project/crudapplication/controller/TodoController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,6 +53,25 @@ public class TodoController {
 
         if (todoOptional.isPresent()) {
             return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Todo not found with id" + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/todos/{id}")
+    public ResponseEntity<?> updateBYId(@PathVariable("id") String id, @RequestBody TodoDTO todo) {
+        Optional<TodoDTO> todoOptional = todoRepo.findById(id);
+
+        if (todoOptional.isPresent()) {
+            TodoDTO todoToSave = todoOptional.get();
+            todoToSave.setCompleted(todo.getCompleted() != null ? todo.getCompleted() : todoToSave.getCompleted());
+            todoToSave.setTodo(todo.getTodo() != null ? todo.getTodo() : todoToSave.getTodo());
+            todoToSave.setDescription(
+                    todo.getDescription() != null ? todo.getDescription() : todoToSave.getDescription());
+            todoToSave.setUpdatedAt(new Date(System.currentTimeMillis()));
+            todoRepo.save(todoToSave);
+
+            return new ResponseEntity<>(todoToSave, HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Todo not found with id" + id, HttpStatus.NOT_FOUND);
         }
