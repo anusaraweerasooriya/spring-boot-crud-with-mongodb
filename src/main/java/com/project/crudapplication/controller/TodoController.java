24f4aecb6ce7bfc3caf3062309a62c38733d82bb
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-04-04 23:34:29 +0530

    Created the REST mapping for getting TODO by ID

--- src/main/java/com/project/crudapplication/controller/TodoController.java
+++ src/main/java/com/project/crudapplication/controller/TodoController.java
@@ -2,12 +2,14 @@ package com.project.crudapplication.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +43,17 @@ public class TodoController {
             return new ResponseEntity<TodoDTO>(todo, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getSingleTodo(@PathVariable("id") String id) {
+        Optional<TodoDTO> todoOptional = todoRepo.findById(id);
+
+        if (todoOptional.isPresent()) {
+            return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Todo not found with id" + id, HttpStatus.NOT_FOUND);
         }
     }
 }
